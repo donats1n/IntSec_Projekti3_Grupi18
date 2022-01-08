fe68705c8da01102309ceefa1b23fa52d0698f42
donats1n <74980340+donats1n@users.noreply.github.com>
2022-01-08 17:15:17 +0100

    Initial files

---
+++ DecrypterPOC-main/DecryptorPOC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DecrypterPOC
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+        }
+
+    }
+}
---
+++ EncrypterPOC-main/WindowsFormsApp1/Encrypter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace RansomwarePOC
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
