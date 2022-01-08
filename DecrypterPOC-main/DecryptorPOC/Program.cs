d1c68fef6d6dd05188382e96424419c05a0bd552
erblinaberisha <75163819+erblinaberisha@users.noreply.github.com>
2022-01-08 20:47:08 +0100

    Update Program.cs

--- DecrypterPOC-main/DecryptorPOC/Program.cs
+++ DecrypterPOC-main/DecryptorPOC/Program.cs
@@ -7,6 +7,22 @@ namespace DecrypterPOC
     class Program
     {
 
+        private const bool DELETE_ENCRYPTED_FILE = true; /* CAUTION */
+        private const bool DECRYPT_DESKTOP = false;
+        private const bool DECRYPT_DOCUMENTS = false;
+        private const bool DECRYPT_PICTURES = false;
+        private const bool DECRYPT_FOLDER = true;
+        private const string ENCRYPTED_FILE_EXTENSION = ".jcrypt";
+        private const string ENCRYPT_PASSWORD = "Password1";
+
+
+        private static string DESKTOP_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        private static string DOCUMENTS_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private static string PICTURES_FOLDER = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        private static string DECRYPTION_LOG = "";
+        private static string RANDOM_FOLDER = "C:/Users/Admin/Desktop/random";
+        private static int decryptedFileCount = 0;
+
         static void Main(string[] args)
         {
            
